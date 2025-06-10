3757d3502de637f80abdab9d2c57726f2513f647
Omar Elsool <omarbaka1999@gmail.com>
2025-06-11 01:22:10 +0300

    Catalog and some Basket code

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndPoints.cs
@@ -0,0 +1,24 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+//public record GetBasketRequest(string UserName); //we don't get request from body
+public record GetBasketResponse(ShoppingCart Cart);
+public class GetBasketEndPoints : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        // first parameter is from the url
+        app.MapGet("/basket/{UserName}", async (string UserName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetBasketQuery(UserName));
+
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Basket")
+        .WithDescription("Get Basket");
+    }
+}
